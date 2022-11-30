0fe70609f284fcfc993dd641dc3d84edc9ddc469
Florencia <florponge@gmail.com>
2022-11-30 19:14:59 -0300

    starting Front

---
+++ client/src/components/landing page/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function LandingPage (){
+return(
+<div>
+    <h1> Bienvenidos a Video Game Store </h1>
+    <Link to ="/home">
+        <button>ENTER</button>
+
+    </Link>
+</div>
+)
+}
